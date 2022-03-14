6aae0858eb3cfe85c71260e6b085fff6cea8670a
hsysr <xiejy.david2014@gmail.com>
2022-03-13 22:29:31 -0400

    add model of inventory

--- mini-amazon-skeleton/app/inventory.py
+++ mini-amazon-skeleton/app/inventory.py
@@ -1,48 +1,51 @@
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import current_user
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, DecimalField
+from flask_wtf import FlaskForm, Form
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, DecimalField, FormField, FieldList
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, NumberRange, InputRequired
 from werkzeug.datastructures import MultiDict
 
 from .models.product import Product
+from .models.inventory import Inventory
 
 from flask import Blueprint
 bp = Blueprint('inventory', __name__)
-products = []
 
 class InventoryForm(FlaskForm):
     quantity = IntegerField('Quantity',
                             validators=[NumberRange(min=0, message='Quantity cannot be negative')], default = 1)
     price = DecimalField('Price',
                          validators=[DataRequired(message='Quantity must be an number'), NumberRange(min=0, message='Price cannot be negative')], default = 1)
-    submit = SubmitField('Change')
+
+class InventoryListForm(FlaskForm):
+	forms = FieldList(FormField(InventoryForm))
 
 @bp.route('/inventory', methods = ['GET', 'POST'])
 def inventory():
-    global products
-    if (len(products) == 0):
-        products = Product.get_all(True)
-    # get all available products for sale:
-    forms = []
-    for product in products:
-        prefix = 'form' + str(product.id)
-        if request.method == 'GET':
-            form = InventoryForm(prefix = prefix, formdata=MultiDict({prefix + '-quantity': product.quantity, prefix + '-price': product.price}))
-        else:
-            form = InventoryForm(prefix = prefix)
-            if not form.submit.data:
-                form = InventoryForm(prefix = prefix, formdata=MultiDict({prefix + '-quantity': product.quantity, prefix + '-price': product.price}))
-        forms.append(form)
-    for i, form in enumerate(forms):
+    if not current_user.is_authenticated:
+        return redirect(url_for('users.login'))
+    inventories = Inventory.get_all_by_sid(current_user.id)
+
+    form = InventoryListForm()
+    if request.method == 'POST':
         if form.validate_on_submit():
-            print("data change:", form.quantity.data, form.price.data)
-            products[i].quantity = form.quantity.data
-            products[i].price = form.price.data
+            for i, iform in enumerate(form.forms):
+                if (inventories[i].quantity != iform.quantity.data):
+                    Inventory.change_quantity(inventories[i].pid, current_user.id, iform.quantity.data)
+                if (inventories[i].price != iform.price.data):
+                    Inventory.change_price(inventories[i].pid, current_user.id, iform.price.data)
+    else:
+        for invent in inventories:
+            form.forms.append_entry()
+            form.forms[-1].quantity.data = invent.quantity
+            form.forms[-1].price.data = invent.price
+
+    names = [Product.get(invent.pid).name for invent in inventories]
 
     return render_template('inventory.html',
-                           inventory_products=products,
-                           forms = forms)
+                           inventory_products=inventories,
+                           form = form,
+                           names = names)
 
 @bp.route('/remove_from_inventory/<id>')
 def remove_from_inventory(id):
---
+++ mini-amazon-skeleton/app/models/inventory.py
@@ -0,0 +1,56 @@
+from flask import current_app as app
+
+
+class Inventory:
+    def __init__(self, pid, sid, quantity, price):
+        self.pid = pid
+        self.sid = sid
+        self.quantity = quantity
+        self.price = price
+
+    @staticmethod
+    def get(pid, sid):
+        rows = app.db.execute('''
+SELECT pid, sid, quantity, price
+FROM Inventory
+WHERE pid = :pid
+AND sid = :sid
+''',
+                              pid=pid,
+                              sid=sid)
+        return Inventory(*(rows[0])) if rows else None
+
+    @staticmethod
+    def get_all_by_sid(sid):
+        rows = app.db.execute('''
+SELECT pid, sid, quantity, price
+FROM Inventory
+WHERE sid = :sid
+ORDER BY pid
+''',
+                              sid=sid)
+        return [Inventory(*row) for row in rows]
+
+    @staticmethod
+    def change_quantity(pid, sid, quantity):
+        app.db.execute('''
+UPDATE Inventory
+SET quantity = :quantity
+WHERE pid = :pid
+AND sid = :sid
+''',
+                              sid=sid,
+                              pid=pid,
+                              quantity=quantity)
+
+    @staticmethod
+    def change_price(pid, sid, price):
+        app.db.execute('''
+UPDATE Inventory
+SET price = :price
+WHERE pid = :pid
+AND sid = :sid
+''',
+                              sid=sid,
+                              pid=pid,
+                              price=price)
